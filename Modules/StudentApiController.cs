4d3c56ef59a1c0527a50038e3d24d717371d22be
unknown <Brainnovation@Janak-PC.braindigit.local>
2020-02-26 11:49:51 +0545

    ajax and jqery

---
+++ Modules/StudentApiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoApp.Student;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Modules
+{
+    [Route("api/student")]
+    [ApiController]
+    public class StudentApiController : ControllerBase
+    {
+        //Get:api/GetAllStudents
+        [HttpGet]
+       
+        public IEnumerable<PersonalDetails>GetAllStudents()
+        {
+            List<PersonalDetails> students = new List<PersonalDetails>
+            {
+                new PersonalDetails
+                {
+                    RegNo="2017-0001",
+                    Name="Janak",
+                    Address="Imadol",
+                    PhoneNo="9813079818",
+                    AdmissionDate=DateTime.Now
+                },
+                new PersonalDetails
+                {
+                    RegNo="2016-00002",
+                    Name="Ashesh",
+                    Address="Lokanthali",
+                    PhoneNo="9813079818",
+                    AdmissionDate=DateTime.Now
+                },
+
+            };
+            return students;
+        }
+       
+    }
+   
+}
---
+++ Student/PersonalDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp.Student
+{
+    public class PersonalDetails
+    {
+        public string RegNo { get; set; }
+        public string Name { get; set; }
+        public string Address{ get; set; }
+        public string PhoneNo { get; set; }
+        public DateTime AdmissionDate { get; set; }
+    }
+}
